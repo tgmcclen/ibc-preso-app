b561519488e143c56c4172984a07093cebe6330d
Trent McClenahan <trent.mcclenahan@gmail.com>
2015-10-06 22:25:26 +1100

    Now retrieves JSON payload from IBC

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var beers = require('./routes/beers');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/beers', beers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/beers.js
+++ routes/beers.js
@@ -4,16 +4,13 @@ var request = require('superagent');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.get('/new', function(req, res, next) {
-  var url = 'https://www.google.com.au/';
+  var url = 'https://beercollector.com.au/beers.json';
 
   request.get(url, function(err, intRes){
     if (err) throw err;
-    res.send(intRes.text);
+    beers = JSON.parse(intRes.text)
+    res.render('beers', { title: 'Beers', beers: beers });
   });
-})
+});
 
 module.exports = router;
